d8b858624ab2f68d44dbccaacddc7f358731d608
Fraamod <amo.fra@live.com>
2025-07-15 11:57:13 +0200

    Create index.js

---
+++ pages/pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Benvenuto su Racely 🚀</h1>
+      <p>La piattaforma che connette motorsport e community.</p>
+    </div>
+  )
+}
